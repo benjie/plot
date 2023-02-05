d9042ecb3713cbea8cbdc37690bc3be768f07658
Benjie Gillam <benjie@jemjie.com>
2023-02-05 12:16:25 +0000

    Working

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const customSketchesEl = document.getElementById("customSketches");
 const canvasEl = document.getElementById("canvas");
 const codeEl = document.getElementById("code");
 const nameEl = document.getElementById("name");
+const errorTextEl = document.getElementById("errorText");
 
 const sketches = [
   {
@@ -17,9 +18,10 @@ return x === y;
   {
     name: "Circle",
     code: `\
-return x ** 2 + y ** 2 = w;
+const r = w;
+return abs(x ** 2 + y ** 2 - r ** 2) < r;
 `,
-    size: 256,
+    size: 257,
     zeroAtCenter: true,
   },
 ];
@@ -34,11 +36,70 @@ for (let i = 0; i < sketches.length; i++) {
 
 let currentSketchIndex = null;
 let currentSketch = null;
+let fn = () => false;
 
 function save() {
   if (currentSketchIndex != null) {
     // TODO: save
   }
+}
+
+function parseCode() {
+  const code = codeEl.value;
+  errorTextEl.innerText = "";
+  try {
+    const { size, zeroAtCenter } = currentSketch;
+    const w = zeroAtCenter ? Math.floor(size / 2) : size;
+    fn = new Function(
+      "w",
+      `\
+${Object.getOwnPropertyNames(Math)
+  .map((k) => `const ${k} = Math.${k};`)
+  .join("\n")}
+return (x, y) => {
+${code}
+}
+`
+    )(w);
+  } catch (e) {
+    console.error(e);
+    errorTextEl.innerText = e.message;
+  }
+}
+
+function draw() {
+  if (!currentSketch) {
+    return;
+  }
+  errorTextEl.innerText = "";
+  const { size, zeroAtCenter } = currentSketch;
+  const offset = zeroAtCenter ? -Math.floor(size / 2) : 0;
+
+  const ctx = canvasEl.getContext("2d");
+  const imageData = ctx.createImageData(size, size);
+
+  outerloop: for (let Y = 0; Y < size; Y++) {
+    const y = size - Y - 1 + offset;
+    for (let X = 0; X < size; X++) {
+      const x = X + offset;
+      const pos = Y * (size * 4) + X * 4;
+      imageData.data[pos + 3] = 255;
+      try {
+        const val = fn(x, y);
+        imageData.data[pos + 0] = val ? 0 : 255;
+        imageData.data[pos + 1] = val ? 0 : 255;
+        imageData.data[pos + 2] = val ? 0 : 255;
+      } catch (e) {
+        imageData.data[pos + 0] = 255;
+        imageData.data[pos + 1] = 0;
+        imageData.data[pos + 2] = 0;
+        console.error(e);
+        errorTextEl.innerText = e.message;
+        break outerloop;
+      }
+    }
+  }
+  ctx.putImageData(imageData, 0, 0);
 }
 
 function loadSketch(sketchId) {
@@ -51,12 +112,24 @@ function loadSketch(sketchId) {
     nameEl.innerText = currentSketch.name;
     codeEl.value = currentSketch.code;
     codeEl.disabled = false;
+    parseCode();
   } else {
     currentSketchIndex = null;
     nameEl.innerHTML = "&mdash;";
     codeEl.value = "";
     codeEl.disabled = true;
+    fn = () => false;
   }
+  draw();
 }
 
 loadSketch(0);
+
+codeEl.addEventListener("change", () => {
+  parseCode();
+  draw();
+});
+codeEl.addEventListener("keyup", () => {
+  parseCode();
+  draw();
+});
